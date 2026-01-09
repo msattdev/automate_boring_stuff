56a2473dba1b42d2ae68f2b5fc8cf4f4d7795170
Matt Satterfield <mattsattdev@gmail.com>
2026-01-09 17:52:01 -0500

    updates

--- Chapter1/firstprogram.py
+++ Chapter1/firstprogram.py
@@ -9,3 +9,5 @@ print(len(my_name)) # Print the length of their name.
 print('What is your age?') # Ask for their age.
 my_age = input('>') 
 print('You will be ' + str(int(my_age) + 1) + ' in a year.') # Tell them how old they will be in a year.
+# print('I eat ' 99 + ' burritos a day.') # This line will cause an error because 99 is an int, not a string.
+print('I eat ' + str(99) + ' burittos a day.') # This line works because we convert 99 into a string first.

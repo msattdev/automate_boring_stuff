2c731e6121b1f61e87052221220cd59085d38853
msattdev <mattsattdev@gmail.com>
2026-01-11 09:24:52 -0500

    hello function added

---
+++ Chapter4/helloFunc.py
@@ -0,0 +1,10 @@
+def hello():
+    # Prints three greeting lines
+    print('Good morning!')
+    print('Good afternoon!')
+    print('Good evening!')
+
+hello()
+hello()
+print('ONE MORE TIME!')
+hello()

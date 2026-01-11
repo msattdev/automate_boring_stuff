2d3ae284c2375c634112b2d5d03dd67303071a70
msattdev <mattsattdev@gmail.com>
2026-01-11 08:57:58 -0500

    fixed indentation

--- Chapter3/rpsGame.py
+++ Chapter3/rpsGame.py
@@ -18,24 +18,45 @@ while True: # This is the main game loop.
             break # Move is valid, so break out of input loop.
         print('Invalid move. Type one of r, p, s or q.')
 
-        # Display the player's chosen move.
-        if player_move == 'r':
-            print('ROCK versus...')
-        elif player_move == 'p':
-            print('PAPER versus...')
-        elif player_move == 's':
-            print('SCISSORS versus...')
+    # Display the player's chosen move.
+    if player_move == 'r':
+        print('ROCK versus...')
+    elif player_move == 'p':
+        print('PAPER versus...')
+    elif player_move == 's':
+        print('SCISSORS versus...')
 
-        # Display the computer's move.
-        move_number = random.randint(1, 3)
-        if move_number == 1:
-            print('ROCK')
-            computer_move = 'r'
-        elif move_number == 2:
-            print('PAPER')
-            computer_move = 'p'
-        elif move_number == 3:
-            print('SCISSORS')
-            computer_move = 's'
+    # Display the computer's move.
+    move_number = random.randint(1, 3)
+    if move_number == 1:
+        print('ROCK')
+        computer_move = 'r'
+    elif move_number == 2:
+        print('PAPER')
+        computer_move = 'p'
+    elif move_number == 3:
+        print('SCISSORS')
+        computer_move = 's'
 
-        # Determine the winner.
+    # Determine the winner.
+    if player_move == computer_move:
+        print('It is a tie!')
+        ties += 1
+    elif player_move == 'r' and computer_move =='s':
+        print('You win!')
+        wins += 1
+    elif player_move == 'p' and computer_move == 'r':
+        print('You win!')
+        wins += 1
+    elif player_move == 's' and computer_move == 'p':
+        print('You win!')
+        wins += 1
+    elif player_move == 'r' and computer_move == 'p':
+        print('You lose!')
+        losses += 1
+    elif player_move == 'p' and computer_move == 's':
+        print('You lose!')
+        losses += 1
+    elif player_move == 's' and computer_move == 'r':
+        print('You lose!')
+        losses += 1

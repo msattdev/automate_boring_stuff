2136a3bae8df7d7e05e7681be155fdd354d3d319
msattdev <mattsattdev@gmail.com>
2026-01-11 09:27:17 -0500

    added func2

---
+++ Chapter4/helloFunc2.py
@@ -0,0 +1,11 @@
+# Prints three greetings to the name provided
+def say_hello_to(name):
+    print(f'Good morning, {name}')
+    print(f'Good afternoon, {name}')
+    print(f'Good evening, {name}')
+
+# Call the function with different names
+say_hello_to('Alice')
+say_hello_to('Bob')
+print('LET\'S TRY ANOTHER NAME!')
+say_hello_to('Charlie')

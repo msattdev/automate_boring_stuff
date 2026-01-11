31f772f0f23c3cb6a9dbe24d52667925604e3d9e
msattdev <mattsattdev@gmail.com>
2026-01-11 09:04:18 -0500

    practice questions

---
+++ Chapter3/practiceForLoop.py
@@ -0,0 +1,7 @@
+# Write a short program that prints the numbers 1 to 10 using a for loop. 
+# Then, write an equivalent program that prints the numbers 1 to 10 using a while loop.
+
+# Using a for loop
+for i in range(1, 11):
+    print(i)
+
---
+++ Chapter3/practiceWhileLoop.py
@@ -0,0 +1,7 @@
+# Write a short program that prints the numbers 1 to 10 using a for loop. 
+# Then, write an equivalent program that prints the numbers 1 to 10 using a while loop.
+
+loop_counter = 1
+while loop_counter < 11:
+    print(loop_counter)
+    loop_counter += 1

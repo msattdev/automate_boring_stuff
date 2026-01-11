2b536990bafdcee9a3cf30adc594676279ffe097
msattdev <mattsattdev@gmail.com>
2026-01-10 20:23:51 -0500

    five times script added

---
+++ Chapter3/fiveTimes.py
@@ -0,0 +1,7 @@
+# Script that prints 5 times, showing the value of i each time.
+# This demonstrates a for loop with range().
+# It also demonstrates that the 0 index is included, but the 5 index is not.
+print('Hello')
+for i in range(5):
+    print('On this iteration, i is set to ' + str(i))
+print('Goodbye!')

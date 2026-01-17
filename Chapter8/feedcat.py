e4f305236dd8c21bdb3b66a408c7c25f0eb5b647
msattdev <mattsattdev@gmail.com>
2026-01-16 19:38:51 -0500

    update

--- Chapter8/feedcat.py
+++ Chapter8/feedcat.py
@@ -5,3 +5,10 @@ Can you feed Eve's cat this weekend?
 
 Sincerely,
 Bob''')
+
+# This is another example of a multi-line comment using triple double quotes.
+"""This is a test Python program.
+Written by Al Sweigart al@inventwithpython.com
+
+This program was designed for Python 3, not Python 2.
+"""
